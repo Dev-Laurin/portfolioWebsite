b9e3f3fc23e3bb4b74331b4c1de9147f71480c07
Dev-Laurin <lafisher@alaska.edu>
2019-01-14 20:28:54 -0900

    Added backend languages.

--- pythonSite/static/js/main.js
+++ pythonSite/static/js/main.js
@@ -3,9 +3,26 @@
 document.addEventListener('DOMContentLoaded', function() {
 	M.AutoInit();
 
+
+
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems, options);
 
+    // var elems = document.querySelectorAll('.modal');
+    // var instances = M.Modal.init(elems, options);
+
+    // var elems = document.querySelectorAll('.carousel');
+    // var instances = M.Carousel.init(elems, options);
+
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
+    var instances = M.Modal.init(elems, {
+        'onOpenEnd': initCarouselModal
+    });
+
+    function initCarouselModal() {
+        var elems = document.querySelectorAll('.carousel');
+        var instances = M.Carousel.init(elems, {'fullWidth': true});
+        instances[0].set(2);
+    }
+    
   }); 
